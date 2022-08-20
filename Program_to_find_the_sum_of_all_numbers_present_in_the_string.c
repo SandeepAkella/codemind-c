356daed043b5d16a54e885a3aac664da3acc86fb
SandeepAkella <97424491+SandeepAkella@users.noreply.github.com>
2022-08-20 12:12:27 +0530

    #include<stdio.h> int main() {     char str[20];     int i,sum=0;     scanf("%s",str);     for (i=0;str[i]!=NULL;i++)     {         if ((str[i]>='0') && (str[i]<='9'))         {             sum=sum+(str[i]-'0');         }     }     printf("%d",sum);     return 0; }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+    char str[20];
+    int i,sum=0;
+    scanf("%s",str);
+    for (i=0;str[i]!=NULL;i++)
+    {
+        if ((str[i]>='0') && (str[i]<='9'))
+        {
+            sum=sum+(str[i]-'0');
+        }   
+    }
+    printf("%d",sum);
+    return 0;
+}
+
